168c01d379204d37c32879fad550d016991988a8
Kamapra18 <marioprayoga18@gmail.com>
2025-06-11 14:07:42 +0700

    add: SEO in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,53 @@ const bebasNeue = Bebas_Neue({
 const lora = Lora({ subsets: ["latin"], variable: "--font-lora" });
 
 export const metadata: Metadata = {
-  title: "Warung Mennara",
-  description: "",
-    icons: {
-    icon: "/favicon.png", 
+  title: {
+    default: "Warung Mennara",
+    template: "%s | Warung Mennara",
+  },
+  description:
+    "Warung Mennara adalah tempat makan terbaik dengan cita rasa khas dan harga bersahabat.",
+  keywords: [
+    "warung",
+    "kuliner",
+    "makanan tradisional",
+    "urutan",
+    "enak",
+    "mennara",
+  ],
+  metadataBase: new URL("https://warungmennara.vercel.app"),
+  openGraph: {
+    title: "Warung Mennara",
+    description:
+      "Kuliner tradisional dengan rasa khas, hanya di Warung Mennara.",
+    url: "https://warungmennara.vercel.app",
+    siteName: "Warung Mennara",
+    images: [
+      {
+        url: "/Logo_Mennara_Fiks.png",
+        width: 1200,
+        height: 630,
+        alt: "Warung Mennara",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/apple-touch-icon.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
 };
 

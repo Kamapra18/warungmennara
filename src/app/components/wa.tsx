6086a31df9492bcc2ade2d3269b3cda588271815
Kamapra18 <marioprayoga18@gmail.com>
2025-06-18 21:47:16 +0700

    perbaikan bug

--- src/app/components/wa.tsx
+++ src/app/components/wa.tsx
@@ -3,7 +3,8 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const WhatsAppFloatingButton: React.FC = () => {
   const phoneNumber = "6281237735005";
-  const message = "Halo, saya ingin tanya dulu tentang produk Warung Mennara, bisa?";
+  const message =
+    "Halo, saya ingin tanya dulu tentang produk Warung Mennara, bisa?";
   const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
     message
   )}`;
@@ -15,7 +16,7 @@ const WhatsAppFloatingButton: React.FC = () => {
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-40 bg-[#25D366] hover:bg-[#1ebe5d] text-white px-4 py-3 rounded-full shadow-lg transition duration-300 flex items-center gap-2">
       <FaWhatsapp size={24} />
-      <span className="hidden md:inline font-medium">Chat saya</span>
+      <span className="hidden md:inline font-medium">Chat Kami</span>
     </a>
   );
 };
